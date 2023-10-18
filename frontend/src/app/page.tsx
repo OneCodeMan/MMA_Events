f0f202b1e700942e23f5baf5aba7d224d5e523a1
Dave Gumba <davegumba@gmail.com>
2023-10-18 04:59:55 -0400

    Basic page set up with info and nav between fight events, need to learn Next.js to style it up

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -29,7 +29,20 @@ function parseDate(dateString: string) {
 }
 
 export default function Home() {
-  const [events, setEvents] = useState<Event[] | null>();
+  const [events, setEvents] = useState([])
+  const [selectedEventIndex, setSelectedEventIndex] = useState(0)
+
+  function nextEvent() {
+    console.log("nextEvent called")
+    let newSelectedEvent = (selectedEventIndex + 1) < events.length ? (selectedEventIndex + 1) : selectedEventIndex
+    setSelectedEventIndex(newSelectedEvent)
+  }
+
+  function previousEvent() {
+    console.log("previousEvent called")
+    let newSelectedEvent = (selectedEventIndex - 1) >= 0 ? (selectedEventIndex - 1) : selectedEventIndex
+    setSelectedEventIndex(newSelectedEvent)
+  }
 
   useEffect(() => {
     const url = 'https://floating-sierra-91917-e404c4f79857.herokuapp.com/events'
@@ -37,25 +50,69 @@ export default function Home() {
       console.log(response)
       var eventsList = response.data
 
-      const sortedEventsListByMostRecent = sortBy(eventsList, (event) => parseDate(event.date));
-      console.log(`Sorted supposedly:\n${sortedEventsListByMostRecent}`)
+      const sortedEventsListByMostRecent = sortBy(eventsList, (event) => parseDate(event.date))
+  
+      // Make sure we only display dates in the future.
+      const yesterdayDate = new Date(new Date().setDate(new Date().getDate() - 1))
+      const filteredSortedEventsOnlyFutureDates = sortedEventsListByMostRecent.filter((event) => parseDate(event.date) > yesterdayDate)
 
-      setEvents(sortedEventsListByMostRecent)
+      setEvents(filteredSortedEventsOnlyFutureDates)
     });
   }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        {events 
-        ? events.map((event, index) => {
-          return (
-            <div key={index}> 
-              <p>{event.title} - {event.date}</p>
-            </div>
-          )
-        })
-        : null}
+
+        <div className="current-event">
+          {events.map((event, index) => {
+            if (index === selectedEventIndex) {
+              return (
+                <div key={index}>
+                  <div id="button-wrapper">
+                  <button type="button" onClick={previousEvent}>Previous</button>
+                  <button type="button" onClick={nextEvent}>Next</button>
+                  </div>
+                  <h1 id="event-title">{event.title}</h1>
+                  <a href={event.event_url}>More info</a>
+                  <h3 id="event-date">{event.date}</h3>
+                  <div id="fight-container">
+                    <ul id="fights-list">
+                      { event.fights ? 
+                        event.fights.map((fight, index) => {
+                          return (
+                              <li key={index}>{fight.fighter_one.name} vs. {fight.fighter_two.name} ({fight.weight})</li>
+                          )
+                        })
+                        : null
+                      }
+                    </ul>
+                  </div>
+                </div>
+              )
+            } else {
+              return null;
+            }
+
+          })}
+        </div>
+
+        <hr />
+        
+        <div id="other-events">
+          {events 
+          ? events.map((event, index) => {
+            if (index !== selectedEventIndex) {
+              return (
+                <div key={index}> 
+                  <p>{event.title} - {event.date}</p>
+                </div>
+              )
+            }
+          })
+          : null}
+        </div>
+        
       </div>
     </main>
   )
