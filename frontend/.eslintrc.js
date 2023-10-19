51dec9bd64c40c099bad4fc3d41af29d410d5a34
Dave Gumba <davegumba@gmail.com>
2023-10-19 13:10:36 -0400

    deleted babel

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "plugin:react/recommended",
+    "extends": [
+        "plugin:react/recommended",
+        "next/core-web-vitals"
+    ],
     "overrides": [
         {
             "env": {
